58d3fa59085b19c00f1d7e07f399fd0fc3f0b71b
Ian Macalinao <me@ian.pw>
2015-04-11 18:42:20 -0400

    Add iapd controllers

--- public/app.js
+++ public/app.js
@@ -1,1 +1,29 @@
-angular.module('justin', []);
+angular.module('justin', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('home', {
+    url: '/',
+    controller: 'HomeCtrl',
+    templateUrl: '/templates/iapds.html'
+  }).state('iapd', {
+    url: '/iapd/:id',
+    controller: 'IapdCtrl',
+    templateUrl: '/templates/iapd.html'
+  });
+
+  $urlRouterProvider.otherwise('/');
+})
+
+.controller('HomeCtrl', function($scope, $http) {
+  $scope.iapds = [];
+  $http.get('/iapds').success(function(data) {
+    $scope.iapds = data;
+  });
+})
+
+.controller('IapdCtrl', function($scope, $stateParams) {
+  $http.get('/iapds/' + $stateParams.id).success(function(data) {
+    $scope.iapd = data;
+  });
+});
+
