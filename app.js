81865a6f446cd2993cc6c4c739d9bd423aacb0e9
Ian Macalinao <me@ian.pw>
2015-04-11 15:10:01 -0400

    Add company aggregation

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 import P from 'bluebird';
 import express from 'express';
-import monk from 'monk';
-
+import mongojs from 'mongojs';
 
 let app = express();
 
 // Database
-let db = monk(process.env.MONGODB_URI || 'mongodb://localhost:27017/bitcamp');
-let iapds = db.get('iapds');
+let db = mongojs(process.env.MONGODB_URI || 'mongodb://localhost:27017/bitcamp');
+let iapds = db.collection('iapds');
 P.promisifyAll(iapds);
 
 app.get('/iapds', (req, res) => {
@@ -25,6 +24,53 @@ app.get('/iapds/:iapd', (req, res) => {
     _id: req.params.iapd
   }).then((doc) => {
     res.json(doc);
+  });
+});
+
+app.get('/iapds/company/:orgpk', (req, res) => {
+  iapds.aggregateAsync([
+    { $limit: 50000 },
+    {
+      $group: {
+        _id: '$CrntEmps.CrntEmp.@orgPK',
+        name: { $first: '$CrntEmps.CrntEmp.@orgNm' },
+        iapds: { $push: '$_id' }
+      }
+    }
+  ]).then((docs) => {
+    let docMap = {};
+
+    // Map documents
+    let ret = docs.filter((doc) => {
+      return !Array.isArray(doc._id);
+    }).map((doc) => {
+      docMap[doc._id] = doc;
+      return doc;
+    });
+
+    // Filter remaining documents
+    docs.filter((doc) => {
+      return Array.isArray(doc._id);
+    }).map((doc) => {
+
+      for (let i = 0; i < doc._id.length; i++) {
+        let _id = doc._id[i];
+        let d = docMap[_id];
+        if (!d) {
+          d = {
+            _id: _id,
+            name: doc.name[i],
+            iapds: []
+          };
+          docMap[_id] = d;
+          ret.push(d);
+        }
+        d.iapds = d.iapds.concat(doc.iapds);
+      }
+
+    });
+
+    res.json(ret);
   });
 });
 
