f8ea263f019c1a17279cf2b8357a65efcb356e94
Justin Ho <ho.justin.c@gmail.com>
2015-04-11 00:47:31 -0400

    Fix script

--- scripts/parse_to_mongodb.py
+++ scripts/parse_to_mongodb.py
@@ -1,13 +1,25 @@
 from pymongo import MongoClient
+import os
 import xmltodict
 
-client = MongoClient()
-col = client.bitcamp.iapds
+client = MongoClient(os.environ['MONGODB_URI'])
+col = client.get_default_database().iapds
 
-for x in xrange(1, 21):
+def chunks(l, n):
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
+
+for x in xrange(1, 4):
     filename = './data/IA_Indvl_Feeds' + str(x) + '.xml'
     print 'Parsing ' + filename + '...'
     res = xmltodict.parse(open(filename))
     indvls = res['IAPDIndividualReport']['Indvls']['Indvl']
-    col.insert_many(indvls)
+    print 'Parsed! Inserting data...'
+
+    ct = 0
+    for c in chunks(indvls, 5):
+        print 'Inserting chunk ' + str(ct)
+        col.insert_many(c)
+        ct += 1
+
     print 'Inserted ' + filename + '.'
