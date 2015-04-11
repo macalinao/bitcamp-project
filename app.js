848b342bf5671797e6c176db30c999d432a06808
Ian Macalinao <me@ian.pw>
2015-04-11 14:15:56 -0400

    json docs

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
+import P from 'bluebird';
 import express from 'express';
+import monk from 'monk';
+
 
 let app = express();
+
+// Database
+let db = monk(process.env.MONGODB_URI || 'mongodb://localhost:27017/bitcamp');
+let iapds = db.get('iapds');
+P.promisifyAll(iapds);
+
+app.get('/iapds', (req, res) => {
+  iapds.findAsync({}, { limit: 50 }).then((docs) => {
+    res.json(docs);
+  });
+});
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
