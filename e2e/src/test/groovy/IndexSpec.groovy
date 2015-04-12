37b9fee5179d91f1c71434730bb2c70fafa7ba93
Ian Macalinao <me@ian.pw>
2015-04-12 04:59:26 -0400

    Add setup and cleanup

--- e2e/src/test/groovy/IndexSpec.groovy
+++ e2e/src/test/groovy/IndexSpec.groovy
@@ -6,9 +6,29 @@ import org.finra.jtaf.ewd.widget.IButton;
 import org.finra.jtaf.ewd.widget.element.html.Button;
 
 class IndexSpec extends Specification {
+  @Shared ewd
+
+  def setupSpec() {
+    ewd = SessionManager.getInstance().getNewSession("client", "client.properties")
+  }
+
+  def cleanupSpec() {
+    ewd.close()
+  }
+
   def "Ensure proper title"() {
-    ExtWebDriver ewd = SessionManager.getInstance().getNewSession("client", "client.properties")
+    setup:
     ewd.open("http://localhost:3000")
+
+    expect:
+    ewd.getTitle() == "InvestorWatch - Verify the integrity of investment advisors"
+  }
+
+  def "Comparison doesn't move pages if investors aren't set"() {
+    setup:
+    ewd.open("http://localhost:3000")
+
+    IButton b = new Button("//*[contains(@class,'btn') and text()='Compare advisors']");
 
     expect:
     ewd.getTitle() == "InvestorWatch - Verify the integrity of investment advisors"
